96309c7e7793b408a1ddbd1664b6a81644759058
angel chelssa <angelchelsa150526@gmail.com>
2024-09-12 23:38:48 +0700

    Add files via upload

---
+++ Latihan03.java
@@ -0,0 +1,7 @@
+public class Latihan03 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama saya Angel chelssa leoniy eka permatasari");
+        System.out.print("\\saya adalah mahasiswa baru jurusan teknologi informasi-POLINEMA");
+    }
+}
---
+++ MyFirstJava03.java
@@ -0,0 +1,7 @@
+public class MyFirstJava03 {
+    public static void main (String [] args) {
+    System.out.println("nama saya: angel chelssa leoniy eka permatasari");
+    System.out.println("saya mahasiswa baru jurusan teknologi informasi");
+    System.out.println("hobi saya renang");
+    }
+} 
